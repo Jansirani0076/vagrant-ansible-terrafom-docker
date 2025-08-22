fb8d04cfee7d0ae2ca411eef1c2ef031927ee0a3
Deenadayalan M <33565852+deenamanick@users.noreply.github.com>
2025-08-22 12:41:50 +0530

    Update install_docker.sh

--- provision/install_docker.sh
+++ provision/install_docker.sh
@@ -14,3 +14,54 @@ usermod -aG docker vagrant
 
 
 chown -R vagrant:vagrant /home/vagrant/terraform-docker
+
+    # Setup Docker workers
+# docker network create --subnet=172.20.0.0/24 ansible-net || true
+# docker pull ubuntu:22.04
+# for i in 1 2; do
+#       cname="worker$i"
+#       cip="172.20.0.1$i"
+#       docker rm -f $cname || true
+#       docker run -d --name $cname --hostname $cname \
+#         --net ansible-net --ip $cip \
+#         --privileged ubuntu:22.04 sleep infinity
+#       docker exec $cname apt-get update
+#       docker exec $cname apt-get install -y openssh-server python3 sudo
+#       docker exec $cname mkdir -p /var/run/sshd /root/.ssh
+#       docker exec $cname bash -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
+#       docker exec $cname service ssh start
+#       pubkey=$(cat /home/vagrant/.ssh/id_rsa.pub)
+#       docker exec $cname bash -c "echo '${pubkey}' >> /root/.ssh/authorized_keys"
+# done
+
+# Setup Docker workers
+docker network create --subnet=172.20.0.0/24 ansible-net || true
+docker pull ubuntu:22.04
+
+pubkey=$(cat /home/vagrant/.ssh/id_rsa.pub)
+
+for i in 1 2; do
+  cname="worker$i"
+  cip="172.20.0.1$i"
+
+  docker rm -f $cname || true
+  docker run -d --name $cname --hostname $cname \
+    --net ansible-net --ip $cip \
+    --privileged ubuntu:22.04 sleep infinity
+
+  docker exec $cname apt-get update
+  docker exec $cname apt-get install -y openssh-server python3 sudo
+
+  docker exec $cname mkdir -p /var/run/sshd /root/.ssh
+  docker exec $cname bash -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
+
+  # Add the VM's pubkey into root's authorized_keys safely
+  echo "$pubkey" | docker exec -i $cname tee /root/.ssh/authorized_keys > /dev/null
+
+  # Fix SSH perms
+  docker exec $cname chmod 700 /root/.ssh
+  docker exec $cname chmod 600 /root/.ssh/authorized_keys
+
+  # Restart ssh service
+  docker exec $cname service ssh restart
+done
